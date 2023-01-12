75a13b55214bfa746e226bbd717bf4f0c9f0ac95
AswinSunil <aswinsunil14@gmail.com>
2023-01-12 16:29:06 +0530

    initial commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ToDoItem(props) {
+  const handleClick = (event) => {
+    if (event.target.style.textDecoration) {
+      event.target.style.removeProperty("text-decoration");
+    } else {
+      event.target.style.setProperty("text-decoration", "line-through");
+    }
+  };
+  return <li onClick={handleClick}>{props.text}</li>;
+}
+
+export default ToDoItem;
